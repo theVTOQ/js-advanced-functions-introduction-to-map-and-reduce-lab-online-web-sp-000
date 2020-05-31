6d766ec6938bc359c332f53ec6625f1c8f1b47d9
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-05-31 23:38:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,11 @@ function mapToNegativize(array){
 function mapToNoChange(array){
   return array;
 }
+
+function mapToDouble(array){
+  const doubledValues = [];
+  array.forEach(num => {
+    doubledValues.push(num * 2);
+  });
+  return doubledValues;
+}
